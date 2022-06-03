8b32de0fa06a2fcf0c7b2ebcbd80991b4ef52cfb
Liyohe <liyohe.cn@gmail.com>
2022-06-03 16:34:10 +0800

    [INI] 初始化

---
+++ src/main/java/cn/liyohe/juc/JucApplication.java
@@ -0,0 +1,16 @@
+package cn.liyohe.juc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Liyohe
+ */
+@SpringBootApplication
+public class JucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/liyohe/juc/JavaSeriesJucApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.liyohe.juc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSeriesJucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
